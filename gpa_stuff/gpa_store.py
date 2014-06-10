ab67369d94cf4ff9a41794f8651c9800b5c1d9f5
mrbmi513 <mrbmi513@gmail.com>
2014-06-10 17:21:51 -0500

    update files

---
+++ gpa_stuff/gpa_store.py
@@ -0,0 +1,26 @@
+grades = []
+def function():
+    global grades
+    expercent = input("Type an example percent float: ")
+    exletter = input("Type an example letter grade: ")
+    expoints = input("Type an example GPA points: ")
+    classtuple = (expercent, exletter, expoints)
+    print("The Tuple for this class is", classtuple)
+    grades.append(classtuple)
+    print("")
+
+def userinput():
+    while True:
+        global classnum
+        classnum = input("How many classes? ")
+        try:
+            classnum = int(classnum)
+            break
+        except:
+            print("That's not an integer. Try Again!")
+userinput()
+for x in range(0, classnum):
+    function()
+print("The list is", grades,"\n")
+print("Your First Class Letter Grade is", grades[0][1])
+input("Press <Enter> To Exit")
